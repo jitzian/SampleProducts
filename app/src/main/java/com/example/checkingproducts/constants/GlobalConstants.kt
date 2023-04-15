3afb7fb8d68dbb193345bd1cd04f156fc0fc6f59
joNA <jitzian@gmail.com>
2023-04-15 13:07:25 -0400

    ibt jc - Initial iteration for global constants file

---
+++ app/src/main/java/com/example/checkingproducts/constants/GlobalConstants.kt
@@ -0,0 +1,10 @@
+package com.example.checkingproducts.constants
+
+class GlobalConstants {
+    companion object {
+
+        //TODO: Keep on adding constants here
+        const val MAX_TIME_OUT = 10000L
+
+    }
+}
