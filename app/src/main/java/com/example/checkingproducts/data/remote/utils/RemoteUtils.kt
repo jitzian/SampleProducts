e71d42ea51bde9647f6c7e9539c4513f74296512
joNA <jitzian@gmail.com>
2023-04-15 12:40:03 -0400

    ibt jc - Creation of Remote Utils that will read the mocked response and it'll provide the contents to the interceptor oce setting up retrofit

---
+++ app/src/main/java/com/example/checkingproducts/data/remote/utils/RemoteUtils.kt
@@ -0,0 +1,23 @@
+package com.example.checkingproducts.data.remote.utils
+
+import android.content.Context
+import android.util.Log
+import com.example.checkingproducts.utils.TAG
+import io.github.brunogabriel.mockpinterceptor.MockPInterceptor
+import okhttp3.OkHttpClient
+/**
+ * Singleton object that will read the mocked response from assets file
+ * */
+object RemoteUtils {
+
+    var mockedContent: String = ""
+
+    fun readMockedContent(context: Context) {
+        val inputStream = context.assets.open("mocks/products.json")
+        val size: Int = inputStream.available()
+        val buffer = ByteArray(size)
+        inputStream.read(buffer)
+        mockedContent = String(buffer)
+    }
+
+}
