cdb0d07b44fd4314dd451358ac7f1a477188f148
joNA <jitzian@gmail.com>
2023-04-15 18:08:05 -0400

    ibt jc - Adding fall-back values

--- app/src/main/java/com/example/checkingproducts/data/remote/model/ProductsItem.kt
+++ app/src/main/java/com/example/checkingproducts/data/remote/model/ProductsItem.kt
@@ -1,10 +1,10 @@
 package com.example.checkingproducts.data.remote.model
 
 data class ProductsItem(
-    val current_value: String,
-    val description: String,
+    val current_value: String = "",
+    val description: String = "",
     val id: String,
-    val name: String,
-    val terms: String,
-    val url: String
+    val name: String = "",
+    val terms: String = "",
+    val url: String? = ""
 )
