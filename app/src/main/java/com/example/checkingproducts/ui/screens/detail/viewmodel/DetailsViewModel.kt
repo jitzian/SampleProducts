4e81b7d4cc26013f2f3d31f55531703ae2da1b7c
joNA <jitzian@gmail.com>
2023-04-16 17:25:50 -0400

    ibt jc - Implementation of fetch product by Id method in VM

--- app/src/main/java/com/example/checkingproducts/ui/screens/detail/viewmodel/DetailsViewModel.kt
+++ app/src/main/java/com/example/checkingproducts/ui/screens/detail/viewmodel/DetailsViewModel.kt
@@ -1,9 +1,29 @@
 package com.example.checkingproducts.ui.screens.detail.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.checkingproducts.data.db.entities.ProductEntityDB
+import com.example.checkingproducts.data.domain.repository.product.ProductRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+@HiltViewModel
+class DetailsViewModel @Inject constructor(
+    private val productRepository: ProductRepository
+) : ViewModel() {
 
-class DetailsViewModel: ViewModel() {
+    private val _state = MutableStateFlow(UIState())
+    val state = _state.asStateFlow()
 
+    fun findProductByIdRoom(id: Int) = viewModelScope.launch(Dispatchers.IO) {
+        productRepository.findProductByIdRoom(id).collect { product ->
+            _state.value = UIState(product)
+        }
+    }
 
+    data class UIState(val data: ProductEntityDB? = null)
 
 }
