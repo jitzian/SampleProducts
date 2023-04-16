3ad2971b4e28cef467584384679897929ff38bca
joNA <jitzian@gmail.com>
2023-04-16 12:03:20 -0400

    ibt jc - Cleanup on unused constants

--- app/src/main/java/com/example/checkingproducts/constants/GlobalConstants.kt
+++ app/src/main/java/com/example/checkingproducts/constants/GlobalConstants.kt
@@ -2,10 +2,6 @@ package com.example.checkingproducts.constants
 
 class GlobalConstants {
     companion object {
-
-        //TODO: Keep on adding constants here
-        const val MAX_TIME_OUT = 10000L
-
         //DB
         const val DB_NAME = "products_db"
         const val DB_VERSION = 1
