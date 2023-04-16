3e7e9eec2380b590cf0dc8cfd3d9c7eeb46fc26e
joNA <jitzian@gmail.com>
2023-04-16 01:19:43 -0400

    ibt jc - Setting up the MainActivity with the navigation flows

--- app/src/main/java/com/example/checkingproducts/MainActivity.kt
+++ app/src/main/java/com/example/checkingproducts/MainActivity.kt
@@ -3,41 +3,32 @@ package com.example.checkingproducts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.checkingproducts.data.remote.utils.RemoteUtils.readMockedContent
+import com.example.checkingproducts.ui.app.ProductsAppScreen
+import com.example.checkingproducts.ui.navigation.MainScreenView
+import com.example.checkingproducts.ui.screens.main.viewmodel.MainViewModel
 import com.example.checkingproducts.ui.theme.CheckingProductsTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalFoundationApi
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        readMockedContent(this)
         setContent {
-            CheckingProductsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            ProductsAppScreen {
+                MainScreenView()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CheckingProductsTheme {
-        Greeting("Android")
     }
 }
