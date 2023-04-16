a3a37cddd162401ce079be2222693e5aebee9dbc
joNA <jitzian@gmail.com>
2023-04-16 14:20:24 -0400

    ibt jc - Code cleanup and adding some documentation

--- app/src/main/java/com/example/checkingproducts/ui/screens/favorites/viewmodel/FavoritesViewModel.kt
+++ app/src/main/java/com/example/checkingproducts/ui/screens/favorites/viewmodel/FavoritesViewModel.kt
@@ -1,19 +1,15 @@
 package com.example.checkingproducts.ui.screens.favorites.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.checkingproducts.data.db.entities.ProductEntityDB
 import com.example.checkingproducts.data.domain.repository.product.ProductRepository
-import com.example.checkingproducts.utils.TAG
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlin.time.Duration.Companion.seconds
 
 @HiltViewModel
 class FavoritesViewModel @Inject constructor(
@@ -23,16 +19,12 @@ class FavoritesViewModel @Inject constructor(
     private val _state = MutableStateFlow(UIState())
     val state = _state.asStateFlow()
 
-    init {
-        //getFavorites()
-    }
-
+    /**
+     * Function that retrieves all the favorite products from DB
+     * */
     fun getFavorites() = viewModelScope.launch(Dispatchers.IO) {
         _state.value = UIState(true)
-        //delay(2.seconds)
-        //productRepository.getAllFavoriteProductsRoom().collect { items ->
-        productRepository.getAllProductsRoom().collect { items ->
-            Log.e(this@FavoritesViewModel.TAG(), "getFavorites: >> $items")
+        productRepository.getAllFavoriteProductsRoom().collect { items ->
             _state.value = UIState(data = items)
         }
     }
