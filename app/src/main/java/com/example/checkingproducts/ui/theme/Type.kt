a2071df90d7e45d1e9867dbc3470acf1846318b5
joNA <jitzian@gmail.com>
2023-04-16 15:40:37 -0400

    ibt jc - Updating Type with the new roboto font type

--- app/src/main/java/com/example/checkingproducts/ui/theme/Type.kt
+++ app/src/main/java/com/example/checkingproducts/ui/theme/Type.kt
@@ -2,17 +2,59 @@ package com.example.checkingproducts.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.checkingproducts.R
+
+val fonts = FontFamily(
+    Font(R.font.roboto_black),
+    Font(R.font.roboto_bold, weight = FontWeight.Bold),
+    Font(R.font.roboto_light, weight = FontWeight.Light),
+    Font(R.font.roboto_thin, weight = FontWeight.Thin),
+    Font(R.font.roboto_italic, weight = FontWeight.Normal, style = FontStyle.Italic)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
+    ),
+    h1 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Thin,
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Light,
+        fontSize = 12.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    caption = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    overline = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Light,
+        fontSize = 10.sp
+    ),
+
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
