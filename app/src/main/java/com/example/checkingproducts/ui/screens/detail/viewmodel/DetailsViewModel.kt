c433bdc61519940d2ff97cf5a9b0d805ed7bc7c2
joNA <jitzian@gmail.com>
2023-04-16 12:01:40 -0400

    ***ibt jc - First iteration of empty composable for DetailsScreen. Just for testing purposes

---
+++ app/src/main/java/com/example/checkingproducts/ui/screens/detail/viewmodel/DetailsViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.checkingproducts.ui.screens.detail.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class DetailsViewModel: ViewModel() {
+
+
+
+}
---
+++ app/src/main/java/com/example/checkingproducts/ui/screens/detail/views/DetailsScreen.kt
@@ -0,0 +1,19 @@
+package com.example.checkingproducts.ui.screens.detail.views
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.checkingproducts.ui.screens.detail.viewmodel.DetailsViewModel
+
+@Composable
+fun DetailsScreenState(
+    id: String,
+    onBackClick: () -> Unit,
+    detailsViewModel: DetailsViewModel = hiltViewModel()
+) {
+
+}
+
+@Composable
+fun DetailsScreen() {
+
+}
