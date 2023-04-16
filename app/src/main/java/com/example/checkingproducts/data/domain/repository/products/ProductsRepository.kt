2cc3ae846833df20445a05ec4049cd14cf3af416
joNA <jitzian@gmail.com>
2023-04-16 14:37:04 -0400

    ibt jc - Removing Product(s) repository since it is not required anymore

--- app/src/main/java/com/example/checkingproducts/data/domain/repository/products/ProductsRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.checkingproducts.data.domain.repository.products
-
-import com.example.checkingproducts.data.remote.model.ProductsItem
-
-interface ProductsRepository {
-    suspend fun getProducts(): List<ProductsItem>
-}
